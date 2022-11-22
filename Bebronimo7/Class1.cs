0aa2fdbbfa5c4b4547e6b02ecd1119b6eeb24f56
unknown <c468aa777@gmail.com>
2022-11-22 22:46:13 +0300

    да

---
+++ Bebronimo7/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bebronimo7
+{
+    public class Cur
+    {
+        public int pos;
+        public int max;
+    }
+    public class Elem
+    {
+        public Elem(string name1, string path1, bool folder1)
+        {
+            name = name1;
+            path = path1;
+            folder = folder1;
+        }
+        public string name;
+        public string path;
+        public bool folder;
+    }
+    public static class Files
+    {
+        public static List<Elem> Get(string path)
+        {
+            List<Elem> elems = new List<Elem>();
+            foreach (string file in Directory.GetFiles(path))
+                elems.Add(new Elem(file.Split("\\")[^1], file, false));
+            foreach (string file in Directory.GetDirectories(path))
+                elems.Add(new Elem(file.Split("\\")[^1], file, true));
+            return elems;
+        }
+    }
+}
---
+++ Bebronimo7/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Bebronimo7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "";
+            List<Elem> elements = new List<Elem>();
+            while (true)
+            {
+                Console.WriteLine("ESC чтобы подняться вверх по проводнику");
+                if (path == "")
+                {
+                    DriveInfo[] allDrives = DriveInfo.GetDrives();
+                    foreach (DriveInfo drive in allDrives)
+                    {
+                        double totalSpace = drive.TotalSize;
+                        double availableSpace = drive.AvailableFreeSpace;
+                        string name = $"Диск {drive.Name}. Доступно {availableSpace} из {totalSpace}";
+                        elements.Add(new Elem(name, drive.Name, true));
+                    }
+                }
+                else
+                {
+                    elements.Clear();
+                    elements = Files.Get(path);
+                }
+                Cur cur = new Cur();
+                cur.pos = 1;
+                cur.max = 0;
+                foreach (Elem elem in elements)
+                {
+                    cur.max++;
+                    Console.WriteLine("  " + elem.name);
+                }
+                bool hehehehaw = true;
+                while (hehehehaw)
+                {
+                    Console.SetCursorPosition(0,cur.pos);
+                    Console.Write(">");
+                    ConsoleKey key = Console.ReadKey().Key;
+                    switch (key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            Console.SetCursorPosition(0, cur.pos);
+                            Console.Write(" ");
+                            if (cur.pos != 1)
+                                cur.pos--;
+                            else
+                                cur.pos = cur.max;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            Console.SetCursorPosition(0, cur.pos);
+                            Console.Write(" ");
+                            if (cur.pos != cur.max)
+                                cur.pos++;
+                            else
+                                cur.pos = 1;
+                            break;
+                        case ConsoleKey.Enter:
+                            hehehehaw = false;
+                            Elem elem = elements[cur.pos - 1];
+                            if (elem.folder)
+                            {
+                                path = elem.path;
+                                Console.Clear();
+                            }
+                            break;
+                        case ConsoleKey.Escape:
+                            while (true)
+                            {
+                                if (path[^1] != '\\')
+                                    path = path.Remove(path.Length - 1);
+                                else
+                                    break;
+                                
+                            }
+                            Console.Clear();
+                            hehehehaw = false;
+                            elements = Files.Get(path);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
